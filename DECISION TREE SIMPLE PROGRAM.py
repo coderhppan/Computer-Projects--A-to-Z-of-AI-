16c6d10c6d035cec986108b19e4f209282bcaa5c
coderhppan <73781052+coderhppan@users.noreply.github.com>
2020-12-07 10:50:52 +0530

    Add files via upload

---
+++ DECISION TREE SIMPLE PROGRAM.py
@@ -0,0 +1,18 @@
+ans1 = input("Has feathers? :")
+if(ans1 == "yes"):
+    ans2 = input("Can it fly? :")
+    if(ans2 == "yes"):
+        print("Your bird is Hawk")
+
+    if(ans2 == "no"):
+        print("Your bird is a Penguin")
+
+if (ans1 == "no"):
+    ans3 = input("Has fins? :")
+
+    if(ans3 == "yes"):
+       print("The fish is a dolphin")
+
+    if(ans3 == "no"):
+       print("The animal is a Bear")
+    
